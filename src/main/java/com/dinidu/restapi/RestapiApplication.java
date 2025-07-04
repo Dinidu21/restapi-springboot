366e171a751c2ff8eb5c56b602e3f761621431ba
Dinidu21 <dinidusachintha3@gmail.com>
2025-07-04 11:02:14 +0530

    feat: enable JPA auditing in RestapiApplication

--- src/main/java/com/dinidu/restapi/RestapiApplication.java
+++ src/main/java/com/dinidu/restapi/RestapiApplication.java
@@ -2,8 +2,10 @@ package com.dinidu.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RestapiApplication {
 
 	public static void main(String[] args) {
